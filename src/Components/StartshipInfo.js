cd2cd70da3bd2374f4bcbf7e153f8a832f2a3239
MariMatallana <marianamatallana@gmail.com>
2023-01-24 00:15:06 +0100

    Ejercicio3-casiOK

--- src/Components/StartshipInfo.js
+++ src/Components/StartshipInfo.js
@@ -17,9 +17,21 @@ const StartshipInfo = () => {
 return (
    
     <div className="container text-white">
-        <h5 className="text-uppercase mx-auto text-center mt-4"></h5>
-        {/* <p>Nombre: {ship.name}</p> */}
-         <p>Modelo: {ship.model}</p>
+        <h5 className="text-uppercase mx-auto text-center mt-4">{ship.name}</h5>
+        <br></br>
+        <p className="text-uppercase mx-auto text-center">FOTO</p>
+         <p className="text-uppercase mx-auto text-center"> <b>Modelo: </b>{ship.model}</p>
+         <p className="text-uppercase mx-auto text-center"> <b>Starships Class: </b>{ship.starship_class}</p>
+         <p className="text-uppercase mx-auto text-center"> <b>Manufacturer: </b>{ship.manufacturer}</p>
+         <p className="text-uppercase mx-auto text-center"> <b>Cost: </b>{ship.cost_in_credits} credits</p>
+         <p className="text-uppercase mx-auto text-center"> <b>Crew: </b>{ship.crew}</p>
+         <p className="text-uppercase mx-auto text-center"> <b>Passengers capacity: </b>{ship.passengers}</p>
+         <p className="text-uppercase mx-auto text-center"> <b>Cargo capacity: </b>{ship.cargo_capacity} tons</p>
+         <p className="text-uppercase mx-auto text-center"> <b>Consumables: </b>{ship.consumables} months</p>
+         <p className="text-uppercase mx-auto text-center"> <b>Length: </b>{ship.length} meters</p>
+         <p className="text-uppercase mx-auto text-center"> <b>Maximun atmospherering speed: </b>{ship.max_atmosphering_speed} km/h</p>
+         <p className="text-uppercase mx-auto text-center"> <b>Hyperdrive rating: </b>{ship.hyperdrive_rating}</p>
+         <p className="text-uppercase mx-auto text-center"> <b>Maximun speed in realspace: </b>{ship.MGLT} mglt</p>
     </div>
     
 )
