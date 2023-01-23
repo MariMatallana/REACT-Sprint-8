24157af05c063a7185f7ab86acd905ad73935297
MariMatallana <marianamatallana@gmail.com>
2023-01-23 11:12:15 +0100

    Avance

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -6,9 +6,19 @@ function Home() {
 
 return (
 
-    <>
-    
-    </>
+    <div>
+        <h4>
+            ¡Bienvenido!
+        </h4> 
+        <p>
+            A continuación encontrarás toda la información de las naves de StarWars. 
+        </p>
+        <p>
+            ¿Estás listo para empezar?
+        </p>
+        
+    </div>
+   
 )
 
 }
